5f9b976f65f2f2a146b775f11e62e3281a8ff504
Saniya Mukhambetkaliyeva <saniya@saniyas-mbp.mynetworksettings.com>
2024-09-08 17:22:28 -0400

    Finished Lab 1 assignment

--- A01/mathgame.c
+++ A01/mathgame.c
@@ -5,7 +5,29 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-  return 0;
+int rounds;
+printf("Welcome to Math Game! \n");
+printf("How many rounds do you want to play? ");
+scanf("%d", &rounds);
+int correct = 0;
+for (int i = 0; i < rounds; i++){
+int n1 = rand() % 10 + 1;
+int n2 = rand() % 10 + 1;
+int result = n1 + n2;
+printf("%d %c %d %s ", n1, '+', n2, "= ?");
+int response;
+scanf("%d", &response);
+if(response == result){
+printf("Correct! \n");
+correct = correct + 1;
+} else {
+printf("Incorrect :( \n");
+correct = correct + 0;
+}
+}
+printf("%s %d %c %d %s", "You anwered", correct, '/', rounds, "correctly. \n");
+return 0;
 }
