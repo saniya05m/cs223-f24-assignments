70216774b0ec34da54a7afcd9d83708bb0c92ead
Saniya Mukhambetkaliyeva <saniya@saniyas-mbp.mynetworksettings.com>
2024-11-01 21:47:09 -0400

    A08 done

--- A08/shell.c
+++ A08/shell.c
@@ -1,5 +1,77 @@
+/*----------------------------------------------
+ * Author: Saniya Mukhambetkaliyeva 
+ * Date: 11/1/2024
+ * Description Shell simulation program that takes in users input and runs a process to execute the commands
+ ---------------------------------------------*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <pwd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+void print_prompt() {
+    char cwd[1024];
+    getcwd(cwd, sizeof(cwd));
+    printf(ANSI_COLOR_GREEN "%s" ANSI_COLOR_RESET " >>> ", cwd);
+}
 
 int main() {
-  return 0;
+    char *input;
+    char *args[100];
+    pid_t pid;
+    int status;
+
+    while (1) {
+        print_prompt();
+        input = readline("");
+
+        if (input == NULL) { // Check for EOF
+            break;
+        }
+
+        if (strcmp(input, "exit") == 0) { // Check for exit
+            free(input);
+            break;
+        }
+
+        if (strlen(input) > 0) {
+            add_history(input); // Add to history
+        }
+
+        int i = 0;
+        args[i] = strtok(input, " ");
+        while (args[i] != NULL) {
+            args[++i] = strtok(NULL, " ");
+        }
+
+        pid = fork();
+        if (pid < 0) {
+            perror("Fork failed");
+        } else if (pid == 0) { // Child process
+            if (execvp(args[0], args) < 0) { // Execute command
+                perror("Command execution failed, sorry :( ");
+                exit(1); // Failed exec
+            }
+        } else {
+            waitpid(pid, &status, 0); // Wait for child
+
+            // Checking child for error
+            if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
+                printf(ANSI_COLOR_RED "Error: Command exited with status %d\n" ANSI_COLOR_RESET, WEXITSTATUS(status));
+            }
+        }
+
+        free(input);
+    }
+    printf("Exiting shell... bye bye :) \n");
+    return 0;
 }
+
--- A08/warmup.c
+++ A08/warmup.c
@@ -1,5 +1,60 @@
+/*----------------------------------------------
+ * Author: Saniya Mukhambetkaliyeva
+ * Date: 10/1/2024
+ * Description: Code that creates multiple children processes and outputs its 
+ * number along with the process number. And then in the end both branches say
+ * bye
+ ---------------------------------------------*/
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main() {
-  return 0;
+    printf("%d] A0\n", getpid());
+
+    pid_t pid_B0 = fork();
+    if (pid_B0 == 0) {
+        // In B0
+        printf("%d] B0\n", getpid());
+
+        pid_t pid_C0 = fork();
+        if (pid_C0 == 0) {
+            // In C0
+            printf("%d] C0\n", getpid());
+            return 0;  // C0 ended
+        }
+
+        // Again in B0
+        wait(NULL);  // Wait for C0 to finish
+
+        pid_t pid_C1 = fork();
+        if (pid_C1 == 0) {
+            // In C1
+            printf("%d] C1\n", getpid());
+            printf("%d] Bye\n", getpid());
+            return 0;  // C1 ends
+        }
+
+        // Again in B0, waiting for C1 to finish
+        wait(NULL);
+        printf("%d] Bye\n", getpid());
+        return 0;  // B0 ends
+    } else {
+        // In the original A0 process
+        pid_t pid_B1 = fork();
+        if (pid_B1 == 0) {
+            // In B1
+            printf("%d] B1\n", getpid());
+            printf("%d] Bye\n", getpid());
+            return 0;  // B1 ends
+        }
+
+        // In A0, waiting for both B0 and B1 to finish
+        wait(NULL);  // Wait for B0
+        wait(NULL);  // Wait for B1
+    }
+
+    return 0;
 }
+
