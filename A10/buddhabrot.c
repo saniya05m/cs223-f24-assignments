a714a1beea8bce1c796fdaa5fa2f46766705c901
Saniya Mukhambetkaliyeva <saniya@n3-8-56.dhcp.drexel.edu>
2024-11-15 18:37:36 -0500

    A10 done

--- A10/buddhabrot.c
+++ A10/buddhabrot.c
@@ -1,3 +1,9 @@
+  /*----------------------------------------------
+ * Author: Saniya Mukhambetkaliyeva
+ * Date: 11/15/2024
+ * Description: Program that uses 4 threads to generate buddhabrot ppm image.
+ ---------------------------------------------*/
+#define _XOPEN_SOURCE 600
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -6,9 +12,190 @@
 #include <pthread.h>
 #include "read_ppm.h"
 #include "write_ppm.h"
+#include <sys/time.h>
+#include <pthread.h> 
+#include <math.h>
+
+pthread_barrier_t barrier;
+pthread_mutex_t mutex;
+
+struct threadArgs {
+  int id;
+  int col0;
+  int row0;
+  int colEnd;
+  int rowEnd;
+  int size;
+  struct ppm_pixel *pixels;
+  float xmin;
+  float xmax;
+  float ymin;
+  float ymax;
+  int maxIterations;
+  int *membership;
+  int *count; 
+  int maxCount;
+};
+
+void *findMember(void* inputData) {
+  // cast input to struct
+  struct threadArgs * myArgs = (struct threadArgs *) inputData;
+
+  // extract variables
+  int col0 = myArgs->col0;
+  int row0 = myArgs->row0;
+  int colEnd = myArgs->colEnd;
+  int rowEnd = myArgs->rowEnd;
+  int size = myArgs->size;
+  float xmin = myArgs->xmin;
+  float xmax = myArgs->xmax;
+  float ymin = myArgs->ymin;
+  float ymax = myArgs->ymax;
+  int maxIterations = myArgs->maxIterations;
+
+  printf("Thread %d sub-image block: cols (%d, %d) to rows (%d, %d)\n", myArgs->id, 
+          col0, colEnd, row0, rowEnd);
+
+  float xfrac, yfrac, x0, y0, x, y, xtemp;
+  int iter;
+  float imageSize = size;
+
+  // Compute image
+  for (int i = row0; i < rowEnd; i++) { // i = rows
+    for (int j = col0; j < colEnd; j++) { // j = cols
+      xfrac = i / imageSize;
+      yfrac = j / imageSize;
+      x0 = xmin + xfrac * (xmax - xmin);
+      y0 = ymin + yfrac * (ymax - ymin);
+
+      x = 0;
+      y = 0;
+      iter = 0;
+
+      while (iter < maxIterations && (x * x + y * y) < 2 * 2) {
+        xtemp = x * x - y * y + x0;
+        y = 2 * x * y + y0;
+        x = xtemp;
+        iter++;
+      }
+      // determine membership
+      if (iter < maxIterations) { // escaped - false
+        myArgs->membership[j * size + i] = 0;
+      }
+      else { // member - true
+        myArgs->membership[j * size + i] = 1;
+      }
+    }
+  }
+  return NULL;
+}
+
+void *findCounts(void* inputData) {
+  // cast input to struct
+  struct threadArgs * myArgs = (struct threadArgs *) inputData;
+
+  // extract variables
+  int col0 = myArgs->col0;
+  int row0 = myArgs->row0;
+  int colEnd = myArgs->colEnd;
+  int rowEnd = myArgs->rowEnd;
+  int size = myArgs->size;
+  float xmin = myArgs->xmin;
+  float xmax = myArgs->xmax;
+  float ymin = myArgs->ymin;
+  float ymax = myArgs->ymax;
+
+  float xfrac, yfrac, x0, y0, x, y, xtemp, yrow, xcol;
+  float imageSize = size;
+  int index = 0;
+
+  for (int i = row0; i < rowEnd; i++) { // i = rows
+    for (int j = col0; j < colEnd; j++) { // j = cols
+      // check membership
+      if (myArgs->membership[j * size + i] == 1) continue;
+
+      xfrac = i / imageSize;
+      yfrac = j / imageSize;
+      x0 = xmin + xfrac * (xmax - xmin);
+      y0 = ymin + yfrac * (ymax - ymin);
+
+      x = 0;
+      y = 0;
+
+      while (x*x + y*y < 2 * 2) {
+        xtemp = x * x - y * y + x0;
+        y = 2 * x * y + y0;
+        x = xtemp;
+
+        yrow = round(size * (y - ymin) / (ymax - ymin));
+        xcol = round(size * (x - xmin)/ (xmax - xmin));
+        if (yrow < 0 || yrow >= size) continue; // out of range
+        if (xcol < 0 || xcol >= size) continue; // out of range9          
+        
+        // update count 
+        index = xcol * size + yrow;
+        myArgs->count[index]++;
+        // update max count
+        if (myArgs->count[index] > myArgs->maxCount){
+          pthread_mutex_lock(&mutex);
+          myArgs->maxCount = myArgs->count[index];
+          pthread_mutex_unlock(&mutex);
+        }
+      } // end of while
+    }
+  }
+  return NULL;
+}
+
+void *computeColors(void* inputData) {
+  float gamma = 0.681;
+  float factor = 1.0/gamma;
+
+  // cast input to struct
+  struct threadArgs * myArgs = (struct threadArgs *) inputData;
+
+  // extract variables
+  int col0 = myArgs->col0;
+  int row0 = myArgs->row0;
+  int colEnd = myArgs->colEnd;
+  int rowEnd = myArgs->rowEnd;
+  int size = myArgs->size;
+
+  float imageSize = size;
+  for (int i = row0; i < rowEnd; i++) { // i = rows
+    for (int j = col0; j < colEnd; j++) { // j = cols
+      float value = 0;
+      if (myArgs->count[j * size + i] > 0) {
+        value = log(myArgs->count[j * size + i]) / log(myArgs->maxCount);
+        value = pow(value, factor);
+      }
+      // save color
+      myArgs->pixels[i * size + j].red = value * 255;
+      myArgs->pixels[i * size + j].green = value * 255;
+      myArgs->pixels[i * size + j].blue = value * 255;
+    }
+  }
+  return NULL;
+}
+
+
+void *start(void* inputData) {
+  struct threadArgs * myArgs = (struct threadArgs *) inputData;
+  
+  // perform step 1
+  findMember(myArgs);
+  // perform step 2
+  findCounts(myArgs);
+  
+  // use a thread barrier to wait for all threads to finish steps 1 and 2
+  pthread_barrier_wait(&barrier);
+  // perform step 3
+  computeColors(myArgs);
+  return NULL;
+}
 
 int main(int argc, char* argv[]) {
-  int size = 480;
+  int size = 800;
   float xmin = -2.0;
   float xmax = 0.47;
   float ymin = -1.12;
@@ -28,11 +215,106 @@ int main(int argc, char* argv[]) {
         "-b <ymin> -t <ymax> -p <numProcesses>\n", argv[0]); break;
     }
   }
-  printf("Generating mandelbrot with size %dx%d\n", size, size);
+  printf("Generating buddhabrot with size %dx%d\n", size, size);
   printf("  Num processes = %d\n", numProcesses);
   printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
   printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
 
-  // todo: your code here
-  // compute image
+  // program timer
+  double timer;
+  struct timeval tStart, tEnd;
+  // start timer
+  gettimeofday(&tStart, NULL);
+
+  struct ppm_pixel* pixels = malloc(sizeof(struct ppm_pixel) * size * size);
+  int* membership = malloc(sizeof(int) * size * size);
+  int* count = malloc(sizeof(int) * size * size);
+  // initialize count
+  for (int i = 0; i < (size * size); i++){
+    count[i] = 0;
+  }
+
+  // set up threads, args, mutex, and barrier
+  pthread_t *threads = malloc(sizeof(pthread_t) * numProcesses);
+  struct threadArgs *tArgs = malloc(sizeof(struct threadArgs) * numProcesses);
+  pthread_barrier_init(&barrier, NULL, numProcesses);
+  pthread_mutex_init(&mutex, NULL);
+
+  // fill in thread args
+  for (int i = 0; i < numProcesses; i++) {
+    //calculate starting & ending locations
+    if (i == 0) {
+      // thread 1
+      tArgs[i].col0 = 0;
+      tArgs[i].row0 = 0;
+      tArgs[i].colEnd = tArgs[i].col0 + size/2;
+      tArgs[i].rowEnd = tArgs[i].row0 + size/2;
+    }
+    else if (i == 1) {
+      // thread 2
+      tArgs[i].col0 = size/2;
+      tArgs[i].row0 = 0;
+      tArgs[i].colEnd = tArgs[i].col0 + size/2;
+      tArgs[i].rowEnd = tArgs[i].row0 + size/2;
+    }
+    else if (i == 2) {
+      // thread 3
+      tArgs[i].col0 = 0;
+      tArgs[i].row0 = size/2;
+      tArgs[i].colEnd = tArgs[i].col0 + size/2;
+      tArgs[i].rowEnd = tArgs[i].row0 + size/2;
+    }
+    else {
+      // thread 4
+      tArgs[i].col0 = size/2;
+      tArgs[i].row0 = size/2;
+      tArgs[i].colEnd = tArgs[i].col0 + size/2;
+      tArgs[i].rowEnd = tArgs[i].row0 + size/2;
+    }
+    // put other data into the args array
+    tArgs[i].id = i + 1;
+    tArgs[i].pixels = pixels;
+    tArgs[i].size = size;
+    tArgs[i].xmin = xmin;
+    tArgs[i].xmax = xmax;
+    tArgs[i].ymin = ymin;
+    tArgs[i].ymax = ymax;
+    tArgs[i].maxIterations = maxIterations;
+    tArgs[i].membership = membership;
+    tArgs[i].count = count;
+    tArgs[i].maxCount = 0;
+  }
+
+  // create threads
+  for (int i = 0; i < numProcesses; i++) {
+    pthread_create(&threads[i], NULL, start, &tArgs[i]);
+  }
+
+  // wait for threads to join
+  for (int i = 0; i < numProcesses; i++) {
+    pthread_join(threads[i], NULL);
+    printf("Thread %d finished\n", (i + 1));
+  }
+
+  pthread_barrier_destroy(&barrier);
+  pthread_mutex_destroy(&mutex);
+
+  gettimeofday(&tEnd, NULL);
+
+  // compute time
+  timer = tEnd.tv_sec - tStart.tv_sec + (tEnd.tv_usec - tStart.tv_usec)/1.e6;
+  printf("Computed buddhabrot set (%dx%d) in %f seconds\n", size, size, timer);
+
+  // write output file
+  char filename[1024];
+  snprintf(filename, 1024, "buddhabrot-%d-%lu.ppm", size, time(0)); 
+  write_ppm(filename, pixels, size, size);
+  printf("Writing file: %s\n", filename);
+
+  free(pixels);
+  free(threads);
+  free(count);
+  free(membership);
+  free(tArgs);
+}
 }
